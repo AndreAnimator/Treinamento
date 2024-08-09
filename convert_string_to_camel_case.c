3136a84c5309bff562aa6f6bdcec52db066a3b9a
André Vitor da Silva Cardoso <98904347+AndreAnimator@users.noreply.github.com>
2024-08-09 17:34:43 -0300

    Update convert_string_to_camel_case.c

--- convert_string_to_camel_case.c
+++ convert_string_to_camel_case.c
@@ -18,43 +18,36 @@ void tester(const char *text, const char *expected) {
     to_camel_case(text, submitted);
 }
 
-void to_camel_case(const char *text, char *camel) {
-    char string[strlen(text)];
-    for(int i=0; i < strlen(text); i++){
-        string[i] = text[i];
+string to_camel_case(const char *text, char *camel) {
+  string solution = "";
+  int end_of_word = 0;
+  for(int i = 0; i < static_cast<int>(text.size()); i++){
+    if(text[i] == 45 || text[i] == 95)
+      end_of_word = 1;
+    else if(end_of_word == 1){
+      end_of_word = 0;
+      if(text[i] >= 97 && text[i] <= 122)
+        solution += text[i] - 32;
+      else
+        solution += text[i]; 
     }
-    for(int i=0; i < strlen(text); i++){
-        camel[i] = text[i];
-    }
-    for(int i=0; i < strlen(string); i++){
-        if(i < strlen(string) - 1){
-            if(string[i] == 45 || string[i] == 95){
-                printf("\n uai");
-                if(string[i+1] > 96 && string[i+1] < 123){
-                    string[i+1] -= 32;
-                    camel[i+1] -= 32;
-                    for(int j = i; j < strlen(string); j++){
-                        camel[j] = string[j+1];
-                    }
-                    for(int u = 0; u < strlen(string); u++){
-                        string[u] = camel[u];
-                        if(u == strlen(string)-1){
-                            string[u] = 0;
-                        }
-                    }
-                }
-                else{
-                    for(int j = i; j < strlen(string); j++){
-                        camel[j] = string[j+1];
-                    }
-                    for(int u = 0; u < strlen(string); u++){
-                        string[u] = camel[u];
-                        if(u == strlen(string)-1){
-                            string[u] = 0;
-                        }
-                    }
-                }
-            }
-        }
-    }
+    else
+      solution += text[i];
+  }
+  return solution;
 }
+
+/*Solução famosa por ser ideal:
+  std::string to_camel_case(std::string text)
+  {
+    for(int i = 0; i < text.size(); ++i)
+    {
+      if(text[i] == '-' || text[i] == '_')
+      {
+        text.erase(i,1);
+        text[i] = toupper(text[i]);
+      }
+    }
+    return text;
+  }
+*/
