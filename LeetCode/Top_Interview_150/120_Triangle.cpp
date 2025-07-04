ec7f692eb58e16a74aa4ef46a1cdbc499e2adabb
André Vitor da Silva Cardoso <98904347+AndreAnimator@users.noreply.github.com>
2025-07-04 17:36:58 -0300

    Create 120_Triangle.cpp

---
+++ LeetCode/Top_Interview_150/120_Triangle.cpp
@@ -0,0 +1,16 @@
+// this solution is not mine and is only intended for educational purposes
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row = triangle.size();
+        vector<int> memo = triangle[row-1];
+
+        for(int r = row-2; r >= 0; r--){
+            for(int c = 0; c <= r; c++){
+                memo[c] = min(memo[c], memo[c+1]) + triangle[r][c];
+            }
+        }
+        
+        return memo[0];
+    }
+};
